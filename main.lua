1650081e878e951b2ed86a84b3af2154a54d262a
Fire Mario Fan <39684412+FireMarioFan@users.noreply.github.com>
2025-07-31 16:46:45 -0400

    Animations
    Thank you kate

--- main.lua
+++ main.lua
@@ -7,6 +7,14 @@ end
 
 local E_MODEL_MISFIRE = smlua_model_util_get_id("misfire_geo")
 
+local MISFIRE_ANIMTABLE = {
+   [charSelect.CS_ANIM_MENU] = MISFIRE_ANIM_CS_IDLE, 
+   [CHAR_ANIM_IDLE_HEAD_LEFT] = MISFIRE_ANIM_IDLE,
+   [CHAR_ANIM_IDLE_HEAD_RIGHT] = MISFIRE_ANIM_IDLE,
+   [CHAR_ANIM_IDLE_HEAD_CENTER] = MISFIRE_ANIM_IDLE,
+   [CHAR_ANIM_RUNNING] = MISFIRE_ANIM_WALK
+}
+
 local PALETTE_MISFIRE = {
     [PANTS] = "DB0900",
     [SHIRT] = "FF5A1F",
@@ -19,3 +27,4 @@ local PALETTE_MISFIRE = {
 
 CT_MISFIRE = _G.charSelect.character_add("Misfire", nil, "KF / Squishy", "FF0000", E_MODEL_MISFIRE, CT_MARIO, nil, 1)
 _G.charSelect.character_add_palette_preset(E_MODEL_MISFIRE, PALETTE_MISFIRE)
+_G.charSelect.character_add_animations(E_MODEL_MISFIRE, MISFIRE_ANIMTABLE)
